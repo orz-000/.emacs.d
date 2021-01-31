a03d8fa534017f19a3f25a5139659f8f262823c2
Shigeki Kobayashi <shigeki.kobayashi@tier4.jp>
2021-01-31 18:50:52 +0900

    don't make bakeup files

--- init.el
+++ init.el
@@ -139,3 +139,7 @@
 ;; neotree（サイドバー）
 (require 'neotree)
 (global-set-key "\C-o" 'neotree-toggle)
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
